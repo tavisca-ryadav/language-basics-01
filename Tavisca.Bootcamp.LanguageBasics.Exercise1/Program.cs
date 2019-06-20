2cdc8bda61c9b9da412d57b9d3ab76ac5ec93cd9
rajat <ryadav@tavisca.in>
2019-06-20 22:43:00 +0530

    solved language basics assignment 01

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,57 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
+            /* Splitting the equation into three parts A,B,C */
+
+            string[] token = equation.Split("=");
+            string[] equ = token[0].Split("*");
+            string a = equ[0],b=equ[1],c=token[1];
+            int an,bn,cn;
+
+            /*First checking if C contains ? character */
+
+            if(c.Contains("?")){
+                an = Convert.ToInt32(a);
+                bn = Convert.ToInt32(b);
+                string cnew= Convert.ToString(an*bn);
+
+                /* findingDigit method will compare two strings  for different characters or length */
+                return findingDigit(cnew,c);
+            }
+
+            /*Checking for A only if it contains '?' if B contains then swap B with A */
+
+            else{
+                if(b.Contains("?")){
+                    string t= a;
+                    a=b;
+                    b=t;
+                }
+                cn = Convert.ToInt32(c);
+                bn = Convert.ToInt32(b);
+                if(cn%bn!=0 || bn==0)
+                    return -1;
+                string anew= Convert.ToString(cn/bn);
+                
+                 /* findingDigit method will compare two strings  for different characters or length */
+                return findingDigit(anew,a);
+            }
             throw new NotImplementedException();
+        }
+
+        public static int findingDigit(string st,string c){
+            int ans = -1;
+            if(st.Length!=c.Length)
+                return ans;
+            for(int i=0;i<c.Length;i++){
+                if(st[i]!=c[i]){
+                    if(c[i].Equals('?')){
+                        ans = st[i]-'0';
+                    }
+                    break;
+                }
+            }
+            return ans;
         }
     }
 }
